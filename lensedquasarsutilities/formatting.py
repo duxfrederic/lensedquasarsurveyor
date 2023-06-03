87412bf9f5e8e06cddbd8d271f9e4f8f7daaee5d
Frédéric Dux <duxfrederic@gmail.com>
2023-06-03 16:45:12 +0200

    gaia star selector

--- lensedquasarsutilities/formatting.py
+++ lensedquasarsutilities/formatting.py
@@ -24,3 +24,11 @@ def get_J2000_name(ra_deg, dec_deg, N=4):
     # Combine the formatted RA and DEC into the 'JHHMM-DDMM' format
     name = f"J{ra_formatted}{dec_formatted}"
     return name
+
+
+def format_coord_deg2hmsdd(ra, dec):
+    # from ra, dec in degrees to the usual HH:MM:SS DD:MM:SS
+    coo = SkyCoord(ra*u.deg, dec*u.deg)
+    rastr = coo.ra.to_string(unit=u.hourangle, sep=":", precision=2, pad=True)
+    decstr = coo.dec.to_string(sep=":", precision=2, alwayssign=True, pad=True)
+    return rastr, decstr
--- lensedquasarsutilities/gaia_utilities.py
+++ lensedquasarsutilities/gaia_utilities.py
@@ -6,6 +6,65 @@ from astroquery.gaia import Gaia
 from lensedquasarsutilities.formatting import get_J2000_name
 
 
+def find_gaia_stars_around_coords(ra, dec, radiusarcsec):
+    """
+
+    :param ra:  float, degrees
+    :param dec:  float, degrees
+    :param radiusarcsec:  float, arcseconds
+    """
+    coord = SkyCoord(ra=ra, dec=dec,
+                     unit=(u.degree, u.degree),
+                     frame='icrs')
+    radius = radiusarcsec * u.arcsec
+    r = Gaia.query_object_async(coordinate=coord, radius=radius)
+
+    return r
+
+
+def get_similar_stars(ra, dec, threshold_distance, mag_estimate=None, verbose=False):
+    """
+
+    :param ra:  float, degrees
+    :param dec:  float, degrees
+    :param threshold_distance:  float, arcseconds
+    :param mag_estimate: float, the typical magnitude of the stars we are looking for.
+    :return:  a list of RAs and a list of Decs of the stars we found.
+
+    """
+    available = find_gaia_stars_around_coords(ra, dec, threshold_distance)
+    available.sort('dist')
+
+    if verbose:
+        print(f"Found {len(available)} potential stars.")
+        print(f"Magnitudes: {[round(e['phot_g_mean_mag'],2) for e in available]}.")
+
+    # so, if we do not provide a magnitude estimate, then we query gaia around our coordinates:
+    # hopefully we will find the magnitude of at least the brightest image of our lensed quasar.
+    if not mag_estimate:
+        veryclose = available[available['dist'] < 2. * u.arcsec.to('degree')]  # at most 2 arcsec away
+
+        if len(veryclose) == 0:
+            raise RuntimeError('No magnitude estimate given, and no gaia detection to get it from.')
+        veryclose.sort('phot_g_mean_mag')
+        mag_estimate = veryclose[0]['phot_g_mean_mag']
+
+    if verbose:
+        print(f"Our magnitude estimate is {mag_estimate:.2f}.")
+
+    # ok, now we just look at what else we have:
+    available = available[available['dist'] > 3. * u.arcsec.to('degree')]
+    good = available[available['phot_g_mean_mag'] < mag_estimate + 1]
+    if verbose:
+        print(f"We have {len(good)} stars of similar magnitudes.")
+
+    # just return the coordinates
+    ras = [g['ra'] for g in good]
+    decs = [g['dec'] for g in good]
+
+    return ras, decs
+
+
 def get_positionangles_separations(ra, dec, searchradiusarcsec, name=None):
 
     """
@@ -89,4 +148,5 @@ def get_positionangles_separations(ra, dec, searchradiusarcsec, name=None):
         return None
 
 
-
+if __name__ == "__main__":
+    ras, decs = get_similar_stars(320.6075, -16.357, 50, verbose=True)
