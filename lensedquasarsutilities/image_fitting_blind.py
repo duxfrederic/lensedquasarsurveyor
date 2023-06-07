a1f5c8b378f4688ff28756897e6bcc7fce764a2f
Frédéric Dux <duxfrederic@gmail.com>
2023-06-08 00:41:24 +0200

    starting cleanup

--- lensedquasarsutilities/image_fitting_blind.py
+++ lensedquasarsutilities/image_fitting_blind.py
@@ -217,8 +217,26 @@ class SimpleLensedQuasarModel:
         self.param_optim_with_galaxy = res.x
         return res.x
 
-    def sample_with_galaxy(self, num_warmup=500, num_samples=500, num_chains=1, position_scale=10.):
+    def sample_with_galaxy(self, num_warmup=500, num_samples=500, num_chains=1,
+                           position_scale=10., positions_prior_type="box",
+                           include_galaxy=True, force_galaxy_between_points=True):
+        """
 
+        :param num_warmup: steps for warmup
+        :param num_samples: steps for actual sampling
+        :param num_chains: number of chain to do in parallel. if gpu, prob. 1 is best unless multiple gpus.
+        :param position_scale: float, default 10 (pixels), scale of the allowed regions for the positions.
+                               Either width of a centered box, or scale of a centered gaussian depending on
+                               `position_prior_type`
+        :param positions_prior_type: string, either "gaussian" or "box".
+        :param include_galaxy: bool, whether we include extra parameters for a Sersic profile in the model.
+        :param force_galaxy_between_points: bool, default True, whether the galaxy is forced to lie somewhere
+                                            between the lensed images or not.
+        :return: numpyro.infer MCMC class used to do the sampling here
+
+        params are updated in class, then you can use the plot functions which will use the medians of the
+        class attributes. (self.param_mediansampler_no_galaxy)
+        """
         def numpyromodel(data, noise):
             # Flatten the images
             image_data_flat = data.flatten()
@@ -232,84 +250,98 @@ class SimpleLensedQuasarModel:
 
             params = {}
 
-            bs = position_scale/2
-            # more likely to be in the center, let's use centered gaussian priors
-            x1 = numpyro.sample('x1', dist.Uniform(-bs, bs))
-            y1 = numpyro.sample('y1', dist.Uniform(-bs, bs))
+            if positions_prior_type == 'box':
+                bs = position_scale/2
 
-            x2 = numpyro.sample('x2', dist.Uniform(-bs, bs))
-            y2 = numpyro.sample('y2', dist.Uniform(-bs, bs))
+                x1 = numpyro.sample('x1', dist.Uniform(-bs, bs))
+                y1 = numpyro.sample('y1', dist.Uniform(-bs, bs))
 
-            # x1 = numpyro.sample('x1', dist.Normal(loc=0., scale=position_scale))
-            # y1 = numpyro.sample('y1', dist.Normal(loc=0., scale=position_scale))
-            #
-            # x2 = numpyro.sample('x2', dist.Normal(loc=0., scale=position_scale))
-            # y2 = numpyro.sample('y2', dist.Normal(loc=0., scale=position_scale))
+                x2 = numpyro.sample('x2', dist.Uniform(-bs, bs))
+                y2 = numpyro.sample('y2', dist.Uniform(-bs, bs))
+            elif positions_prior_type == 'gaussian':
+                x1 = numpyro.sample('x1', dist.Normal(loc=0., scale=position_scale))
+                y1 = numpyro.sample('y1', dist.Normal(loc=0., scale=position_scale))
+
+                x2 = numpyro.sample('x2', dist.Normal(loc=0., scale=position_scale))
+                y2 = numpyro.sample('y2', dist.Normal(loc=0., scale=position_scale))
 
             params['positions'] = (x1, y1, x2, y2)
-
-            # ok, let's condition xg, yg to lie somewhere between the point sources
-            angle = numpyro.deterministic("angle", jnp.arctan2(jnp.array([y2 - y1]), jnp.array([x2 - x1]))[0])
-            length = numpyro.deterministic("length", ((x2-x1)**2 + (y2-y1)**2)**0.5)
-            scale = numpyro.sample("scale", dist.Uniform(0.2, 0.8))  # "between" source 1 and source 2.
-            xg = numpyro.deterministic("xg",  x1 + jnp.cos(angle) * length * scale)
-            yg = numpyro.deterministic("yg",  y1 + jnp.sin(angle) * length * scale)
-
-            # xg = numpyro.sample('xg', dist.Normal(loc=0., scale=position_scale))
-            # yg = numpyro.sample('yg', dist.Normal(loc=0., scale=position_scale))
-
-            params['galparams'] = {}
-            params['galparams']['positions'] = (xg, yg)
-
-            r_e = numpyro.sample('r_e', dist.Uniform(0.5, 4.))
-            ellip = numpyro.sample('ellip', dist.Uniform(0., 0.9))
-            theta = numpyro.sample('theta', dist.Uniform(0, 2*np.pi))
-            n = numpyro.sample('n', dist.Normal(loc=4.0, scale=0.5))
-            params['galparams']['morphology'] = r_e, n, ellip, theta
-
-            params['galparams']['I_e'] = {}
-            for i, band in enumerate(self.bands):
-                A1 = numpyro.sample(f'A1_{band}', dist.Uniform(5., 140.))
-                A2 = numpyro.sample(f'A2_{band}', dist.Uniform(5., 140.))
+            # ok, now populate the band-dependant params:
+            for band in self.bands:
+                A1 = numpyro.sample(f'A1_{band}', dist.Uniform(5., 140.))  # since we normalize our data, this range
+                A2 = numpyro.sample(f'A2_{band}', dist.Uniform(5., 140.))  # should be fine ...
                 params[band] = (A1, A2)
-                params['galparams'][f'I_e_{band}'] = numpyro.sample(f'I_e_{band}', dist.Uniform(0., 30.))
-
-                dx = numpyro.sample(f'dx_{band}', dist.Uniform(-0.1, 0.1))
-                dy = numpyro.sample(f'dy_{band}', dist.Uniform(-0.1, 0.1))
+                dx = numpyro.sample(f'dx_{band}', dist.Uniform(-0.1, 0.1))  # had gaussian prior here, but
+                dy = numpyro.sample(f'dy_{band}', dist.Uniform(-0.1, 0.1))  # was getting insane translations
                 params[f'offsets_{band}'] = dx, dy
 
-            mod = self.model_with_galaxy(params)
+            if not include_galaxy:
+                # then we stop here and use the simple model.
+                mod = self.model_no_galaxy(params)
+
+            # buuut more stuff if we add a galaxy.
+            if include_galaxy:
+
+                if force_galaxy_between_points:
+                    # ok, let's condition xg, yg to lie somewhere between the point sources
+                    angle = numpyro.deterministic("angle", jnp.arctan2(jnp.array([y2 - y1]), jnp.array([x2 - x1]))[0])
+                    length = numpyro.deterministic("length", ((x2-x1)**2 + (y2-y1)**2)**0.5)
+                    scale = numpyro.sample("scale", dist.Uniform(0.2, 0.8))  # "between" source 1 and source 2.
+                    xg = numpyro.deterministic("xg",  x1 + jnp.cos(angle) * length * scale)
+                    yg = numpyro.deterministic("yg",  y1 + jnp.sin(angle) * length * scale)
+                else:
+                    xg = numpyro.sample('xg', dist.Normal(loc=0., scale=position_scale))
+                    yg = numpyro.sample('yg', dist.Normal(loc=0., scale=position_scale))
+
+                params['galparams'] = {}
+                params['galparams']['positions'] = (xg, yg)
+
+                # sersic profile params
+                r_e = numpyro.sample('r_e', dist.Uniform(0.5, 4.))
+                ellip = numpyro.sample('ellip', dist.Uniform(0., 0.9))
+                theta = numpyro.sample('theta', dist.Uniform(0, 2*np.pi))
+                n = numpyro.sample('n', dist.Normal(loc=4.0, scale=0.5))
+                params['galparams']['morphology'] = r_e, n, ellip, theta
+
+                # band dependant params
+                params['galparams']['I_e'] = {}
+                for band in self.bands:
+                    params['galparams'][f'I_e_{band}'] = numpyro.sample(f'I_e_{band}', dist.Uniform(0., 30.))
+
+                mod = self.model_with_galaxy(params)
 
             # likelihood, gaussian errors
             with numpyro.plate('data', len(image_data_flat)):
                 numpyro.sample('obs', dist.Normal(mod.flatten(), image_uncertainties_flat), obs=image_data_flat)
 
-        # run MCMC
-        nuts_kernel = numpyro.infer.BarkerMH(numpyromodel)
-        mcmc = numpyro.infer.MCMC(nuts_kernel, num_warmup=num_warmup, num_samples=num_samples, num_chains=num_chains)
+        # run MCMC, this barkerMH kernel seems to be working very well.
+        # NUTS was getting stuck too much.
+        kernel = numpyro.infer.BarkerMH(numpyromodel)
+        mcmc = numpyro.infer.MCMC(kernel, num_warmup=num_warmup, num_samples=num_samples, num_chains=num_chains)
         rng_key = random.PRNGKey(0)
         mcmc.run(rng_key, self.data, self.noisemap)
         mcmc.print_summary()
 
+        # and now the great unpacking.
         pps = {'galparams': {}}
         medians = {k: np.median(val) for k, val in mcmc.get_samples().items()}
 
         pps['positions'] = [medians[k] for k in ('x1', 'y1', 'x2', 'y2')]
-        if 'xg' not in medians:
-            # then we forced the midpoint
-            x1, y1, x2, y2 = pps['positions']
-            xg, yg = 0.5 * (x1 + x2), 0.5 * (y1 + y2)
-            pps['galparams']['positions'] = xg, yg
-        else:
-            pps['galparams']['positions'] = [medians[k] for k in ('xg', 'yg')]
-        pps['galparams']['morphology'] = [medians[k] for k in ('r_e', 'n', 'ellip', 'theta')]
 
-        for i, band in enumerate(self.bands):
+        for band in self.bands:
             pps[band] = [medians[k] for k in (f'A1_{band}', f'A2_{band}')]
-            pps['galparams'][f'I_e_{band}'] = medians[f'I_e_{band}']
             pps[f'offsets_{band}'] = [medians[k] for k in (f'dx_{band}', f'dy_{band}')]
 
-        self.param_mediansampler_with_galaxy = pps
+        if include_galaxy:
+            for band in self.bands:
+                pps['galparams'][f'I_e_{band}'] = medians[f'I_e_{band}']
+            pps['galparams']['positions'] = [medians[k] for k in ('xg', 'yg')]
+            pps['galparams']['morphology'] = [medians[k] for k in ('r_e', 'n', 'ellip', 'theta')]
+            # store the medians
+            self.param_mediansampler_with_galaxy = pps
+        else:
+            self.param_mediansampler_no_galaxy = pps
+
         return mcmc
 
     def sample_no_galaxy(self, num_warmup=500, num_samples=500, num_chains=1, position_scale=5.):
@@ -392,7 +424,7 @@ class SimpleLensedQuasarModel:
         offsy = (self.X.shape[0] - 1) / 2.
         x1, x2 = x1 + offsx, x2 + offsx
         y1, y2 = y1 + offsy, y2 + offsy
-        if 'galparams' in params:
+        if 'galparams' in params and 'positions' in params['galparams']:
             xg, yg = params['galparams']['positions']
             xg += offsx
             yg += offsy
@@ -410,7 +442,7 @@ class SimpleLensedQuasarModel:
                 axs[i, j].axis('off')
                 axs[i, j].set_title(f'{band} {title}')
                 axs[i, j].plot([x1+dx, x2+dx], [y1+dy, y2+dy], 'x', color='red')
-                if 'galparams' in params:
+                if 'galparams' in params and 'positions' in params['galparams']:
                     axs[i, j].plot([xg+dx], [yg+dy], 'x', color='orange')
         plt.tight_layout()
         return fig, axs
@@ -472,71 +504,22 @@ if __name__ == "__main__":
     ff = "/tmp/test/cutouts_legacysurvey_J2122-1621_cutouts.h5"
     # ff = '/tmp/test/cutouts_panstarrs_J2122-1621_cutouts.h5'
     modelm = prepare_model_from_h5(ff)
-    # param =  {'positions': (5, 5, -3, -5), 'g': (200., 105.), 'r': (100., 130.),
-    #                                        'i': (90.0, 100.), 'z': (80.0, 100.)}
-    # hi = modelm.model_no_galaxy(param)
-    # hi2 = np.moveaxis(hi, 0, 2)
-    # hi2 /= np.max(hi2)
-    # plt.imshow(hi2, origin='lower')
-    # plt.show()
 
-    # param = {'positions': (0, 0, 0, 0), 'g': (200., 105.), 'r': (100., 130.),
-    #                                        'i': (90.0, 100.), 'z': (80.0, 100.),
-    #          'galparams': {'positions': (0., 0.), 'morphology': (10., 1.5, 0.1, 0.2),
-    #                        'I_e': {'r': 0.0001, 'i': 0.005, 'g': 0.005, 'z': 0.006}
-    #           }
-    #          }
-    #
-    # hi2 = np.moveaxis(hi, 0, 2)
-    # hi2 /= np.max(hi2)
-    # plt.imshow(hi2, origin='lower')
-    # plt.show()
-    #
-    # out = modelm.sample_with_galaxy(num_samples=100, num_warmup=100)
-    # modelm.plot_model_with_galaxy()
-    # modelm.plot_model_with_galaxy()
+    modelm.param_mediansampler_with_galaxy = {'galparams': {'positions': [0, 0], 'morphology': [1.0710126, 1., 0.0, 0.],
+                                                            'I_e_g.00000': 0.5, 'I_e_i.00000': .1,
+                                                            'I_e_r.00000': 2.0, 'I_e_z.00000': 5.0},
+                                              'positions': [0., 1., 0., 1.], 'g.00000': [0., 0.],
+                                              'offsets_g.00000': [0.0, 0.0], 'i.00000': [1., 1.],
+                                              'offsets_i.00000': [0.0, 0.0], 'r.00000': [1., 1.],
+                                              'offsets_r.00000': [0.0, 0.0], 'z.00000': [1., 1.],
+                                              'offsets_z.00000': [0.0, 0.0]}
+    modelm.param_mediansampler_no_galaxy = {'positions': [0., 1., 0., 0.], 'g.00000': [1., 1.],
+                                            'offsets_g.00000': [0.0, 0.0], 'i.00000': [1., 1.],
+                                            'offsets_i.00000': [0.0, 0.0], 'r.00000': [1., 1.],
+                                            'offsets_r.00000': [0.0, 0.0], 'z.00000': [1., 1.],
+                                            'offsets_z.00000': [0.0, 0.0]}
 
-    modelm.param_mediansampler_with_galaxy = {'galparams': {'positions': [0, 0], 'morphology': [1.0710126, 1, 0.0,0],
-                                                            'I_e_g.00000': 0.5, 'I_e_i.00000': .1, 'I_e_r.00000': 2.0, 'I_e_z.00000': 5.0},
-                                              'positions': [0, 10, 0, -10], 'g.00000': [0., 0.],
-                                              'offsets_g.00000': [0., 0.], 'i.00000': [1., 1.],
-                                              'offsets_i.00000': [0., -0.0], 'r.00000': [1., 1.],
-                                              'offsets_r.00000': [0.0, -0.0], 'z.00000': [1.0, 1.],
-                                              'offsets_z.00000': [0.0, -0.0]}
-    modelm.param_mediansampler_no_galaxy = {  'positions': [0, 10, 0, 0], 'g.00000': [1., 1.],
-                                              'offsets_g.00000': [0., 0.], 'i.00000': [1., 1.],
-                                              'offsets_i.00000': [0., -0.0], 'r.00000': [1., 1.],
-                                              'offsets_r.00000': [0.0, -0.0], 'z.00000': [1.0, 1.],
-                                              'offsets_z.00000': [0.0, -0.0]}
-
-    # out = modelm.sample_no_galaxy(num_warmup=1000, num_samples=500, position_scale=10.0)
-    # modelm.plot_model_no_galaxy()
-    out = modelm.sample_with_galaxy(num_warmup=4000, num_samples=2500, position_scale=15.0)
+    out = modelm.sample_with_galaxy(num_warmup=2000, num_samples=1500, position_scale=15.0, include_galaxy=False)
     print(modelm.param_mediansampler_with_galaxy)
-    modelm.plot_model_with_galaxy()
+    modelm.plot_model_no_galaxy()
     plt.show()
-"""
-if __name__ == '__main__':
-        # pass
-        psf = lambda x, y, x0, y0, A: A * np.exp(-0.022 * (x - x0)**2 - 0.02 * (y - y0)**2)
-
-        # grid of small pixels
-        X, Y = x, y = np.meshgrid(np.linspace(-32, 32, 64), np.linspace(-32, 32, 64))
-
-        narrow_psf = psf(x, y, 0, 0, 1)
-        narrow_psf /= narrow_psf.sum()
-        modc = SimpleLensedQuasarModel(X, X, narrow_psf, 2, [], [])
-        params = [0, 6, 2, 0, -6, 3, 0, 0, 0.00, 5.0, 1.5, 0.5, 30]
-        m = modc.model_with_galaxy(params)
-
-        noise_scale = 0.001
-        data = m
-        data += np.random.normal(loc=0, scale=noise_scale, size=data.shape)
-        plt.imshow(data)
-        noisemap = noise_scale * np.ones_like(data)
-        modc.data = data
-        modc.noisemap = noisemap
-
-        plt.show()
-
-#"""
